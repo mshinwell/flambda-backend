cc408663ecf69531582ee6359275e556a38f0c1f
Mark Shinwell <mshinwell@gmail.com>
2021-01-29 12:51:47 +0000

    Comparison script

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script should be run on Linux.
+
 set -eu -o pipefail
 
 upstream_tree=$(pwd)/ocaml-install
@@ -35,11 +37,13 @@ archives_to_compare="\
   libstr_stubs_native.a
   "
 
+# We don't currently check dynlink.a because the build process is quite
+# different and we currently have a source code patch in the Flambda backend
+# to work around limitations of Dune.
+
 # compiler-libs/ocamlmiddleend.a is not built in the Flambda backend.
 # We should try to remove this from upstream by fixing the ocamlobjinfo
 # problem.
-
-# dynlink.a
 
 upstream_filename_of_archive_member () {
   filename=$1
@@ -195,7 +199,82 @@ compare_archive () {
   rm -f $flambda_backend_contents
 }
 
+# 1. Check immediate subdirs of installation root match (just the names of
+# the subdirs, not the contents).
+
+# The Flambda backend does not build or install man pages.
+upstream_subdirs=$(ls -1 $upstream_tree | grep -v '^man$')
+flambda_backend_subdirs=$(ls -1 $flambda_backend_tree)
+
+if [ "$upstream_subdirs" != "$flambda_backend_subdirs" ]; then
+  echo -e "Subdirs of install tree don't match, expected:\n$upstream_subdirs"
+  exit 1
+fi
+
+# 2. Check that everything provided upstream in the bin/ directory that we
+# want is present.
+
+upstream_bin=$(ls $upstream_tree/bin \
+  | grep -v '^ocamlcp$' \
+  | grep -v '^ocamlcp.byte$' \
+  | grep -v '^ocamlcp.opt$' \
+  | grep -v '^ocamloptp$' \
+  | grep -v '^ocamloptp.byte$' \
+  | grep -v '^ocamloptp.opt$' \
+  | grep -v '^ocamlprof$' \
+  | grep -v '^ocamlprof.byte$' \
+  | grep -v '^ocamlprof.opt$' \
+  )
+
+flambda_backend_bin=$(ls $flambda_backend_tree/bin)
+
+if [ "$upstream_bin" != "$flambda_backend_bin" ]; then
+  echo "Executables in bin/ don't match:"
+  patdiff <(echo $upstream_bin) <(echo $flambda_backend_bin)
+  exit 1
+fi
+
+# 3. Make sure executables in lib/ocaml/ are present.
+
+lib_exes="expunge extract_crc objinfo_helper"
+
+for exe in $lib_exes; do
+  if [ ! -f $upstream_tree/lib/ocaml/$exe ]; then
+    echo "Executable $exe in lib/ocaml/ not found in upstream tree"
+    exit 1
+  fi
+  if [ ! -f $flambda_backend_tree/lib/ocaml/$exe ]; then
+    echo "Executable $exe in lib/ocaml/ not found in Flambda backend tree"
+    exit 1
+  fi
+done
+
+# 4. Check that immediate subdirs of lib/ match (just the names, not contents).
+
+upstream_subdirs=$(ls -1 $upstream_tree/lib)
+flambda_backend_subdirs=$(ls -1 $flambda_backend_tree/lib)
+
+if [ "$upstream_subdirs" != "$flambda_backend_subdirs" ]; then
+  echo -e "Subdirs of install tree don't match, expected:\n$upstream_subdirs"
+  exit 1
+fi
+
+# 5. Check that the VERSION files match.
+
+upstream_version=$(cat $upstream_tree/lib/ocaml/VERSION)
+flambda_backend_version=$(cat $flambda_backend_tree/lib/ocaml/VERSION)
+
+if [ "$upstream_version" != "$flambda_backend_version" ]; then
+  echo -e "VERSION files in lib/ocaml/ do not match:"
+  patdiff <(echo $upstream_version) <(echo $flambda_backend_version)
+  exit 1
+fi
+
+# 6. Check .a files match (archive filenames, archive member filenames, symbols
+# defined by archive members).  Some rewrites are performed as above.
+
 for archive in $archives_to_compare; do
   compare_archive $archive
 done
 
+# 7. 
