b10e22952e136e9516d8957c47229f28c3fb8940
Mark Shinwell <mshinwell@gmail.com>
2021-09-23 15:40:01 +0100

    Introduce No_alias_or_equals

--- middle_end/flambda2/types/type_descr.rec.ml
+++ middle_end/flambda2/types/type_descr.rec.ml
@@ -29,56 +29,7 @@ module Make
               with type typing_env_extension := Typing_env_extension.t
               with type type_grammar := Type_grammar.t) =
 struct
-  module Descr = struct
-    type t =
-      | No_alias of Head.t Or_unknown_or_bottom.t
-      | Equals of Simple.t
-
-    let print ppf t =
-      let colour = Flambda_colours.top_or_bottom_type () in
-      match t with
-      | No_alias Unknown ->
-        if Flambda_features.unicode ()
-        then
-          Format.fprintf ppf "@<0>%s@<1>\u{22a4}@<0>%s" colour
-            (Flambda_colours.normal ())
-        else
-          Format.fprintf ppf "@<0>%sT@<0>%s" colour (Flambda_colours.normal ())
-      | No_alias Bottom ->
-        if Flambda_features.unicode ()
-        then
-          Format.fprintf ppf "@<0>%s@<1>\u{22a5}@<0>%s" colour
-            (Flambda_colours.normal ())
-        else
-          Format.fprintf ppf "@<0>%s_|_@<0>%s" colour
-            (Flambda_colours.normal ())
-      | No_alias (Ok head) -> Head.print ppf head
-      | Equals simple ->
-        Format.fprintf ppf "@[(@<0>%s=@<0>%s %a)@]" (Flambda_colours.error ())
-          (Flambda_colours.normal ())
-          Simple.print simple
-
-    let apply_renaming t renaming =
-      if Renaming.is_empty renaming
-      then t
-      else
-        match t with
-        | No_alias Bottom | No_alias Unknown -> t
-        | No_alias (Ok head) ->
-          let head' = Head.apply_renaming head renaming in
-          if head == head' then t else No_alias (Ok head')
-        | Equals simple ->
-          let simple' = Simple.apply_renaming simple renaming in
-          if simple == simple' then t else Equals simple'
-
-    let free_names t =
-      match t with
-      | No_alias Bottom | No_alias Unknown -> Name_occurrences.empty
-      | No_alias (Ok head) -> Head.free_names head
-      | Equals simple ->
-        Name_occurrences.downgrade_occurrences_at_strictly_greater_kind
-          (Simple.free_names simple) Name_mode.in_types
-  end
+  module Descr = No_alias_or_equals.Make (Head)
 
   (* CR mshinwell: Flambda 2 compilation is causing calls to e.g. [descr] to be
      indirect. *)
