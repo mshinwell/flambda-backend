dd9c0f7855023d32e34521b7912cfac6c928d6a5
Mark Shinwell <mshinwell@pm.me>
2022-07-25 15:23:30 +0100

    Revert test case changes

--- ocaml/testsuite/tests/runtime-errors/stackoverflow.ml
+++ ocaml/testsuite/tests/runtime-errors/stackoverflow.ml
@@ -23,29 +23,23 @@ script = "sh ${test_source_directory}/has-stackoverflow-detection.sh"
 
 *)
 
-let [@inline never] rec f x =
+let rec f x =
   if not (x = 0 || x = 10000 || x = 20000)
   then 1 + f (x + 1)
   else
     try
-      let [@inline never] recurse () =
-        1 + f (x + 1)
-      in
-      let result = recurse () in  (* prevent tail call *)
-      Sys.opaque_identity result
+      1 + f (x + 1)
     with Stack_overflow ->
       print_string "x = "; print_int x; print_newline();
       raise Stack_overflow
 
 let _ =
- let p = Sys.opaque_identity (ref 42) in
  begin
   try
     ignore(f 0)
   with Stack_overflow ->
     print_string "Stack overflow caught"; print_newline()
  end ;
- for i = 1 to 1000 do ignore (Sys.opaque_identity (ref 1_000_000)) done;
  (* GPR#1289 *)
  Printexc.record_backtrace true;
  begin
@@ -53,5 +47,4 @@ let _ =
     ignore(f 0)
   with Stack_overflow ->
     print_string "second Stack overflow caught"; print_newline()
- end;
- print_string "!p = "; print_int !p; print_newline ()
+ end
