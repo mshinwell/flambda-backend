a38e70672eaffe937c4e43308e7a62a93356c99e
Mark Shinwell <mshinwell@pm.me>
2023-03-22 12:46:40 +0000

    work

--- up.ml
+++ up.ml
@@ -1,6 +1,6 @@
-external make_unboxed_pair_v_v : 'a -> 'b -> ('a, 'b) unboxed_pair
-external unboxed_pair_field_0_v_v : ('a, 'b) unboxed_pair -> 'a
-external unboxed_pair_field_1_v_v : ('a, 'b) unboxed_pair -> 'b
+external make_unboxed_pair_v_v : 'a -> 'b -> ('a, 'b) unboxed_pair = "%make_unboxed_pair_v_v"
+external unboxed_pair_field_0_v_v : ('a, 'b) unboxed_pair -> 'a = "%unboxed_pair_field_0_v_v"
+external unboxed_pair_field_1_v_v : ('a, 'b) unboxed_pair -> 'b = "%unboxed_pair_field_1_v_v"
 
 let f i x y =
   let p = make_unboxed_pair_v_v x y in
