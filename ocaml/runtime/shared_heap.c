bdd7694f4c41515427d44d9e22b896ea83cd9fc1
Mark Shinwell <mshinwell@pm.me>
2024-02-07 16:57:17 +0000

    pf

--- ocaml/runtime/shared_heap.c
+++ ocaml/runtime/shared_heap.c
@@ -570,7 +570,7 @@ Caml_inline intnat prefetch_pool(pool* pool, sizeclass sz, int first_half)
   header_t* start = POOL_FIRST_BLOCK(pool, sz);
   header_t* end = POOL_END(pool);
   // * 3 / 4 instead of / 2 is a fair bit worse for misses
-  intnat half_in_words = (intnat) ((end - start) * 1 / 4);
+  intnat half_in_words = (intnat) ((end - start) / 2);
   intnat work = end - start;
 
   if (first_half) end -= half_in_words;
@@ -580,7 +580,7 @@ Caml_inline intnat prefetch_pool(pool* pool, sizeclass sz, int first_half)
 
   while (p < end) {
     caml_prefetch(p);
-    p += 64 / sizeof(header_t);
+    p += 128 / sizeof(header_t);
   }
 
   return work;
@@ -606,7 +606,7 @@ Caml_inline intnat prefetch_pool2(pool* pool, sizeclass sz, int first_half)
 
   while (p < end) {
     caml_prefetch(p);
-    p += 64 / sizeof(header_t);
+    p += 128 / sizeof(header_t);
   }
 
   return work;
