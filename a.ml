d27227260c7e8daec36ad0c935e4776504f70194
Mark Shinwell <mshinwell@pm.me>
2024-06-03 12:22:55 +0100

    examples

---
+++ a.ml
@@ -0,0 +1,8 @@
+external ( + ) : int -> int -> int = "%addint"
+
+let x = 1
+
+let foo y = y + x
+
+let bar z = z + 1
+
---
+++ b.ml
@@ -0,0 +1,7 @@
+external ( + ) : int -> int -> int = "%addint"
+
+let x_in_b = A.x
+
+let foo_in_b y = A.foo y + 2
+
+let bar_in_b z = A.bar z
