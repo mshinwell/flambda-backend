8b07ef7abb67b3340c22f4b31b4683210384f08e
Mark Shinwell <mshinwell@pm.me>
2024-06-03 12:26:59 +0100

    update b.ml

--- b.ml
+++ b.ml
@@ -5,3 +5,11 @@ let x_in_b = A.x
 let foo_in_b y = A.foo y + 2
 
 let bar_in_b z = A.bar z
+
+module type S = sig
+  val x : int
+  val foo : int -> int
+  val bar : int -> int
+end
+
+let mod_A = (module A : S)
