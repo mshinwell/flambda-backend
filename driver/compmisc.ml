250a66fbbec5c862603014c0b384a9dbf06014a6
David Allsopp <david.allsopp@metastack.com>
2024-07-05 09:26:24 +0100

    Add Compmisc.reinit_path
    Compmisc.init_path initialises the load path using
    Config.standard_library. Compmisc.reinit_path generalises this, allowing
    an alternate value for the standard library location to be used. This is
    used internally when testing compiler installations in order to allow
    Ccomp.call_linker to be used.
    
    Deviations from original:
      - Paranoid implementation using Compmisc.reinit_path used instead of
        the change to Compmisc.init_path merged in OCaml 5.5
    
    (cherry picked from commit 6b82c95cf44071e86c3cbba2757520d0c227f339)

--- driver/compmisc.ml
+++ driver/compmisc.ml
@@ -28,7 +28,9 @@ let auto_include find_in_dir fn =
    then the directories specified with the -H option (in command line order).
  *)
 
-let init_path ?(auto_include=auto_include) ?(dir="") () =
+let reinit_path ?(auto_include=auto_include)
+                ?(standard_library=Config.standard_library)
+                ?(dir="") () =
   let visible =
     if !Clflags.use_threads then "+threads" :: !Clflags.include_dirs
     else
@@ -42,14 +44,17 @@ let init_path ?(auto_include=auto_include) ?(dir="") () =
        !Compenv.first_include_dirs]
   in
   let visible =
-    List.map (Misc.expand_directory Config.standard_library) visible
+    List.map (Misc.expand_directory standard_library) visible
   in
   let visible =
+    let std_include =
+      if !Clflags.no_std_include then [] else [standard_library]
+    in
     (if !Clflags.no_cwd then [] else [dir])
-    @ List.rev_append visible (Clflags.std_include_dir ())
+    @ List.rev_append visible std_include
   in
   let hidden =
-    List.rev_map (Misc.expand_directory Config.standard_library)
+    List.rev_map (Misc.expand_directory standard_library)
       !Clflags.hidden_include_dirs
   in
   Load_path.init ~auto_include ~visible ~hidden;
@@ -65,6 +70,8 @@ let init_parameters () =
         Env.register_parameter param
     )
     param_names
+
+let init_path = reinit_path ?standard_library:None
 
 (* Return the initial environment in which compilation proceeds. *)
 
--- driver/compmisc.mli
+++ driver/compmisc.mli
@@ -16,6 +16,8 @@
 val init_path :
   ?auto_include:Load_path.auto_include_callback -> ?dir:string -> unit -> unit
 val init_parameters : unit -> unit
+val reinit_path : ?auto_include:Load_path.auto_include_callback
+    -> ?standard_library:string -> ?dir:string -> unit -> unit
 val initial_env : unit -> Env.t
 
 (* Support for flags that can also be set from an environment variable *)
