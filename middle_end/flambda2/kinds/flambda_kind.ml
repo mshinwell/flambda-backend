25224ad077c5a6a33a5eff4f22cf127036e851e0
Mark Shinwell <mshinwell@pm.me>
2024-09-23 13:37:42 +0100

    Unboxed products in arrays

--- middle_end/flambda2/kinds/flambda_kind.ml
+++ middle_end/flambda2/kinds/flambda_kind.ml
@@ -586,6 +586,9 @@ module With_subkind = struct
       | Unboxed_int64_array
       | Unboxed_nativeint_array
       | Unboxed_vec128_array
+      | Unboxed_product_array
+    (* CR mshinwell: more information could be added to
+       [Unboxed_product_array] *)
 
     (* CR vlaviron: only [Value] kinds need [value_subkind] and [nullable]
        fields. We should switch this to a variant in the future to avoid
@@ -614,7 +617,8 @@ module With_subkind = struct
       | Unboxed_int32_array, Unboxed_int32_array
       | Unboxed_int64_array, Unboxed_int64_array
       | Unboxed_nativeint_array, Unboxed_nativeint_array
-      | Unboxed_vec128_array, Unboxed_vec128_array ->
+      | Unboxed_vec128_array, Unboxed_vec128_array
+      | Unboxed_product_array, Unboxed_product_array ->
         true
       | ( Variant { consts = consts1; non_consts = non_consts1 },
           Variant { consts = consts2; non_consts = non_consts2 } ) ->
@@ -665,8 +669,8 @@ module With_subkind = struct
           | Boxed_nativeint | Boxed_vec128 | Tagged_immediate | Variant _
           | Float_block _ | Float_array | Immediate_array | Value_array
           | Generic_array | Unboxed_float32_array | Unboxed_int32_array
-          | Unboxed_int64_array | Unboxed_nativeint_array | Unboxed_vec128_array
-            ),
+          | Unboxed_int64_array | Unboxed_nativeint_array
+          | Unboxed_product_array | Unboxed_vec128_array ),
           _ ) ->
         false
 
@@ -736,6 +740,9 @@ module With_subkind = struct
         | Unboxed_vec128_array ->
           Format.fprintf ppf "%t=Unboxed_vec128_array%t" colour
             Flambda_colours.pop
+        | Unboxed_product_array ->
+          Format.fprintf ppf "%t=Unboxed_product_array%t" colour
+            Flambda_colours.pop
 
       let compare = Stdlib.compare
 
@@ -761,7 +768,7 @@ module With_subkind = struct
           | Float_block _ | Float_array | Immediate_array | Value_array
           | Generic_array | Unboxed_float32_array | Unboxed_int32_array
           | Unboxed_int64_array | Unboxed_nativeint_array | Unboxed_vec128_array
-            ),
+          | Unboxed_product_array ),
           _ ) ->
         Misc.fatal_errorf "Subkind %a is not valid for kind %a"
           Non_null_value_subkind.print value_subkind print kind
@@ -769,10 +776,7 @@ module With_subkind = struct
       ));
     { kind; value_subkind; nullable }
 
-  let anything kind =
-    match kind with
-    | Value -> create kind Anything Nullable
-    | Naked_number _ | Region | Rec_info -> create kind Anything Non_nullable
+  let anything kind = create kind Anything
 
   let compatible (t : t) ~(when_used_at : t) =
     equal t.kind when_used_at.kind
@@ -828,6 +832,8 @@ module With_subkind = struct
   let generic_array = create value Generic_array Non_nullable
 
   let unboxed_vec128_array = create value Unboxed_vec128_array Non_nullable
+
+  let unboxed_product_array = create value Unboxed_product_array Non_nullable
 
   let block tag fields =
     if List.exists (fun (t : t) -> not (equal t.kind Value)) fields
@@ -958,7 +964,7 @@ module With_subkind = struct
       | Parrayval (Punboxedintarray Pnativeint) -> Unboxed_nativeint_array
       | Parrayval (Punboxedvectorarray Pvec128) -> Unboxed_vec128_array
       | Parrayval (Pgcscannableproductarray _ | Pgcignorableproductarray _) ->
-        Misc.fatal_errorf "Flambda_kind.from_lambda_value_kind: unimplemented"
+        unboxed_product_array
     in
     let nullable : Nullable.t =
       match vk.nullable with
@@ -997,7 +1003,7 @@ module With_subkind = struct
           | Float_block _ | Float_array | Immediate_array | Value_array
           | Generic_array | Unboxed_float32_array | Unboxed_int32_array
           | Unboxed_int64_array | Unboxed_nativeint_array | Unboxed_vec128_array
-            ),
+          | Unboxed_product_array ),
           Non_nullable )
       | (Naked_number _ | Region | Rec_info), _, Nullable ->
         assert false
@@ -1025,23 +1031,16 @@ module With_subkind = struct
   end)
 
   let has_useful_subkind_info (t : t) =
-    match t.kind with
-    | Value -> (
-      match t.value_subkind with
-      | Anything -> (
-        match t.nullable with Nullable -> false | Non_nullable -> true)
-      | Boxed_float | Boxed_float32 | Boxed_int32 | Boxed_int64
-      | Boxed_nativeint | Boxed_vec128 | Tagged_immediate | Variant _
-      | Float_block _ | Float_array | Immediate_array | Value_array
-      | Generic_array | Unboxed_float32_array | Unboxed_int32_array
-      | Unboxed_int64_array | Unboxed_nativeint_array | Unboxed_vec128_array ->
-        true)
-    | Naked_number _ | Rec_info | Region -> false
+    match t.subkind with
+    | Anything -> false
+    | Boxed_float | Boxed_float32 | Boxed_int32 | Boxed_int64 | Boxed_nativeint
+    | Boxed_vec128 | Tagged_immediate | Variant _ | Float_block _ | Float_array
+    | Immediate_array | Value_array | Generic_array | Unboxed_float32_array
+    | Unboxed_int32_array | Unboxed_int64_array | Unboxed_nativeint_array
+    | Unboxed_vec128_array | Unboxed_product_array ->
+      true
 
-  let erase_subkind (t : t) : t =
-    match t.kind with
-    | Value -> { t with value_subkind = Anything; nullable = Nullable }
-    | Naked_number _ | Rec_info | Region -> t
+  let erase_subkind (t : t) : t = { t with subkind = Anything }
 
   let equal_ignoring_subkind t1 t2 = equal (erase_subkind t1) (erase_subkind t2)
 
@@ -1054,7 +1053,7 @@ module With_subkind = struct
       | Boxed_nativeint | Boxed_vec128 | Variant _ | Float_block _ | Float_array
       | Immediate_array | Value_array | Generic_array | Unboxed_float32_array
       | Unboxed_int32_array | Unboxed_int64_array | Unboxed_nativeint_array
-      | Unboxed_vec128_array ->
+      | Unboxed_vec128_array | Unboxed_product_array ->
         true)
     | Naked_number _ | Region | Rec_info -> false
 
