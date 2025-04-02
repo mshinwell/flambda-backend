c2be6e5f1199a7f6c5940f10f4c2cb60b46ed2f6
Mark Shinwell <mshinwell@pm.me>
2025-04-02 10:29:26 +0100

    remove old mli

--- backend/cfg_selectgen_target_intf.ml
+++ backend/cfg_selectgen_target_intf.ml
@@ -1,6 +1,4 @@
-(******************************************************************************
- *                             flambda-backend                                *
- * -------------------------------------------------------------------------- *
+(* -------------------------------------------------------------------------- *
  *                               MIT License                                  *
  *                                                                            *
  * Copyright (c) 2025 Jane Street Group LLC                                   *
@@ -25,11 +23,47 @@
  * DEALINGS IN THE SOFTWARE.                                                  *
  ******************************************************************************)
 
-(** Instruction selection, parameterised over the target machine. *)
+(** Interface to be satisfied by target-specific code, for instruction
+    selection. *)
 
-module Make (Target : Cfg_selectgen_target_intf.S) : sig
-  val emit_fundecl :
-    future_funcnames:Misc.Stdlib.String.Set.t ->
-    Cmm.fundecl ->
-    Cfg_with_layout.t
+module type S = sig
+  val is_immediate :
+    Simple_operation.integer_operation ->
+    int ->
+    Select_utils.is_immediate_result
+
+  val is_immediate_test :
+    Simple_operation.integer_comparison ->
+    int ->
+    Select_utils.is_immediate_result
+
+  val is_simple_expr : Cmm.expression -> Select_utils.is_simple_expr_result
+
+  val effects_of : Cmm.expression -> Select_utils.effects_of_result
+
+  val select_addressing :
+    Cmm.memory_chunk -> Cmm.expression -> Arch.addressing_mode * Cmm.expression
+
+  val select_operation :
+    Cmm.operation ->
+    Cmm.expression list ->
+    label_after:Label.t ->
+    Select_utils.select_operation_result
+
+  val select_store :
+    is_assign:bool ->
+    Arch.addressing_mode ->
+    Cmm.expression ->
+    Select_utils.select_store_result
+
+  val is_store_out_of_range :
+    Cmm.memory_chunk ->
+    byte_offset:int ->
+    Select_utils.is_store_out_of_range_result
+
+  val insert_move_extcall_arg :
+    Cmm.exttype ->
+    Reg.t array ->
+    Reg.t array ->
+    Select_utils.insert_move_extcall_arg_result
 end
