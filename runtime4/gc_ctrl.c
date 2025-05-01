2805ba9296c414a4c1178b813cd1b25a1e582a7d
Mark Shinwell <mshinwell@pm.me>
2025-05-01 19:29:18 +0100

    be careful of TERRIBLE HACKS

--- runtime4/gc_ctrl.c
+++ runtime4/gc_ctrl.c
@@ -282,12 +282,17 @@ extern value caml_enable_debug_file (void);
 
 CAMLprim value caml_gc_stat(value v)
 {
-  value result;
-  CAML_EV_BEGIN(EV_EXPLICIT_GC_STAT);
-  if (v != Val_unit) caml_enable_debug_file ();
-  result = heap_stats (1);
-  CAML_EV_END(EV_EXPLICIT_GC_STAT);
-  return result;
+  if (v != Val_unit) {
+    caml_enable_debug_file ();
+    return Val_unit;
+  }
+  else {
+    value result;
+    CAML_EV_BEGIN(EV_EXPLICIT_GC_STAT);
+    result = heap_stats (1);
+    CAML_EV_END(EV_EXPLICIT_GC_STAT);
+    return result;
+  }
 }
 
 CAMLprim value caml_gc_quick_stat(value v)
