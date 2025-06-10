992a625e128745512e0f70fb6f952f7df8531879
Mark Shinwell <mshinwell@pm.me>
2025-06-10 19:21:24 +0100

    extensions

--- scripts/setup-playground.sh
+++ scripts/setup-playground.sh
@@ -2,7 +2,7 @@
 
 set -eu -o pipefail
 
-apt-get install opam
+sudo apt-get install opam
 
 opam switch create 5.2.0+ox \
   --repos "with-extensions=\
@@ -11,3 +11,15 @@ git+https://github.com/janestreet/opam-repository.git#with-extensions,default"
 eval $(opam env --switch 5.2.0+ox)
 
 opam install ocamlformat merlin ocaml-lsp-server utop
+
+# Install extensions here rather than via customizations.vscode.extensions
+# in .devcontainer.json, so the OPAM environment is set up first, to avoid
+# various popups etc.
+code="$(ls ~/.vscode-server*/bin/*/bin/code-server* | head -n 1)"
+if [ ! -z "$code" ]; then
+  $code --install-extension ocamllabs.ocaml-platform
+  $code --install-extension ms-vscode.cpptools
+  $code --install-extension eamodio.gitlens
+else
+  ls -lR ~ | grep code-server
+fi
