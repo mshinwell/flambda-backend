7315ea4171dc8d85705097bee6ca4d9a086ed5ac
Mark Shinwell <mshinwell@pm.me>
2025-12-19 12:33:38 +0000

    Linker flags ordering

--- asmcomp/asmlink.ml
+++ asmcomp/asmlink.ml
@@ -238,12 +238,15 @@ let call_linker ?dissector_args file_list_rev startup_file output_name =
         then ""
         else Config.native_c_libraries
       in
-      (* Filter ccobjs to only include linker flags (starting with -l) *)
+      (* Filter ccobjs to only include linker flags (starting with -).
+         These flags (e.g. -l:libfoo.a) must come before partition files
+         on the command line so that static libraries are available when
+         the linker processes the partition object files. *)
       let linker_flags =
         List.filter (fun s -> String.length s > 0 && s.[0] = '-')
           (List.rev !Clflags.ccobjs)
       in
-      Build_linker_args.object_files args @ linker_flags, c_lib
+      linker_flags @ Build_linker_args.object_files args, c_lib
     | None ->
       (* Normal mode: combine startup + ml_objfiles + ccobjs + runtime_lib *)
       let file_list_rev =
